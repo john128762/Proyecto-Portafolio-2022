0c823c3c2e16fae0c01779ce659319bf8f3c2a2b
Rafael Segura <ra.segura@duocuc.cl>
2022-05-09 18:45:50 -0400

    Agregados modelos a models.py de backend

--- laTiaDelPan/backend/models.py
+++ laTiaDelPan/backend/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+    PROV_RUT = models.IntegerField(primary_key=True)
+    PROV_DV = models.CharField(max_length = 1)
+    PROV_NOMBRE = models.CharField(max_length=40)
+    PROV_CONTACTO = models.CharField(max_length=40)
+
+class Categoria(models.Model):
+    CAT_ID = models.AutoField()
+    CAT_NOMBRE = models.CharField(max_length=20)
+
+class Usuario (models.Model):
+    USU_RUT = models.IntegerField(primary_key=True)
+    USU_DV = models.CharField(max_length=1)
+    USU_USERNAME = models.CharField(max_length = 20)
+    USU_NOMBRES = models.CharField(max_length = 40)
+    USU_APELLIDOS = models.CharField(max_length = 40)
+    USU_PASSWORD = models.CharField(max_length = 40)
+    USU_VIGENCIA = models.BooleanField()
+    USU_ADMINISTRADOR = models.BooleanField()
+    USU_FECHA_INGRESO = models.DateTimeField()
+    USU_FECHA_MODIFICACION = models.DateTimeField()
+
+class Boleta (models.Model):
+    BOL_NUMERO = models.AutoField()
+    BOL_FECHA_VENTA = models.DateTimeField()
+    BOL_SUBTOTAL = models.DecimalField()
+    BOL_IVA = models.DecimalField()
+    BOL_VIGENCIA = models.BooleanField()
+    USU_RUT = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+class Producto(models.Model):
+    PROD_CODIGO = models.CharField(max_length = 15, primary_key=True)
+    PROD_NOMBRE = models.CharField(max_length=40)
+    PROD_VALOR = models.DecimalField()
+    PROD_STOCK = models.IntegerField()
+    PROV_RUT = models.ForeignKey(Proveedor, on_delete=models.RESTRICT)
+    CAT_ID = models.ForeignKey(Categoria, on_delete=models.RESTRICT)
+
+class Detalle_Boleta(models.Model):
+    BOL_NUMERO = models.ForeignKey(Boleta, on_delete=models.RESTRICT)
+    PROD_CODIGO = models.ForeignKey(Producto, on_delete=models.RESTRICT)
+    DET_CANTIDAD = models.IntegerField()
+    DET_VALOR = models.DecimalField()
+
+class Fact_Proveedor(models.Model):
+    FAC_NUMERO = models.AutoField()
+    FAC_FECHA_VENTA = models.DateTimeField()
+    FAC_TOTAL = models.DecimalField()
+
+class Det_Fact_Proveedor(models.Model):
+    FAC_NUMERO = models.ForeignKey(Fact_Proveedor, on_delete=models.RESTRICT)
+    PROD_CODIGO = models.ForeignKey(Producto, on_delete=models.RESTRICT)
+    DFT_CANTIDAD = models.IntegerField()
+    DFT_VALOR = models.DecimalField()

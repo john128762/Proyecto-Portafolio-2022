55c0fdab4c4d0f66b01d46f51f9a752b3c6e74a7
john <johncamp1410@gmail.com>
2022-06-03 22:52:15 -0400

    mantenedor usuario con problemas para editar

--- laTiaDelPan/backend/Controladores/ControladorUsuarios.py
+++ laTiaDelPan/backend/Controladores/ControladorUsuarios.py
@@ -72,8 +72,8 @@ class ControladorUsuarios:
         if isinstance(usuario, modelsApp.Usuario):
             try:
                 resUsuario = models.Usuario(models.Usuario.objects.get(USU_USERNAME = usuario.Username))
-                resUsuario.USU_RUT = usuario.RUT.split("-")[0]
-                resUsuario.USU_DV = usuario.RUT.split("-")[1]
+                resUsuario.USU_RUT = usuario.RUT.split("-")[0].replace(".","")
+                resUsuario.USU_DV = usuario.RUT.split("-")[1].replace(".","")
                 resUsuario.USU_PASSWORD = usuario.Password
                 resUsuario.USU_NOMBRES = usuario.Nombres
                 resUsuario.USU_APELLIDOS = usuario.Apellidos
